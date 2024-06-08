95dcdecee4e23c3dca3ba27099358993f4672576
Dmitriy <dshestaev@yandex.ru>
2024-06-08 15:24:54 +0300

    Добавлены аннотации сущностей для User & Item

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -5,6 +5,10 @@ import lombok.*;
 import lombok.experimental.FieldDefaults;
 import ru.practicum.shareit.request.ItemRequest;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
@@ -13,14 +17,21 @@ import javax.validation.constraints.NotNull;
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Builder
+@Entity
+@Table(name = "items")
 public class Item {
+    @Id
     Integer id;
+    @Column(name = "name", nullable = false)
     @NotBlank
     String name;
+    @Column(name = "description", nullable = false)
     @NotBlank
     String description;
+    @Column(name = "available", nullable = false)
     @NotNull
     Boolean available;
+    @Column(name = "owner_id", nullable = false)
     Integer ownerId;
     ItemRequest request;
 }
--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -3,6 +3,10 @@ package ru.practicum.shareit.user;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
@@ -11,10 +15,15 @@ import javax.validation.constraints.NotBlank;
 @AllArgsConstructor
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
     Integer id;
+    @Column(name = "name", nullable = false)
     @NotBlank
     String name;
+    @Column(name = "email", nullable = false)
     @NotBlank
     @Email
     String email;
